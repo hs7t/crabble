6f2a58c4291f1bb6394076c8f437c0a9492d0ce2
Alí Sinisterra <haidarconh@gmail.com>
2025-11-01 18:55:00 -0500

    finish Shuffler container

---
+++ crabble-web/src/lib/Shuffler.svelte
@@ -0,0 +1,14 @@
+<div class="main-container">
+    <div class="word">
+        test
+    </div>
+</div>
+
+<style>
+    .main-container {
+        width: 100%;
+    }
+    .main-container .word {
+        padding: 1ch;
+    }
+</style>
