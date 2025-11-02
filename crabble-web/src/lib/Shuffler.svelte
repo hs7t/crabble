8e2ee459fa5a813f8ee54569ebb0a20cbd1437d9
Alí Sinisterra <haidarconh@gmail.com>
2025-11-02 16:56:45 -0500

    add word swapping with keyboard in Shuffler

--- crabble-web/src/lib/Shuffler.svelte
+++ crabble-web/src/lib/Shuffler.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
+    import { swap } from "../shared.svelte";
     import WordBlock from "./components/WordBlock.svelte";
     import { dndzone } from 'svelte-dnd-action'
 
-    let words = [{id: 1, title: "meow"}, {id: 2, title: "woo"}, {id: 3, title: "hoo"}]
+
+    type Word = {
+        id: number
+        title: string
+    }
+    let words: Array<Word> = [{id: 1, title: "meow"}, {id: 2, title: "woo"}, {id: 3, title: "hoo"}]
 
 	function handleSort(e: any) {
 		words = e.detail.items;
 	}
 
+    function handleBlockInteract(word: Word) {
+        let indexInArray = words.findIndex(w => w.id === word.id)
+        words = swap(words, indexInArray, ((indexInArray + 1) % words.length))
+    }
 </script>
 
 <div use:dndzone="{{items: words}}" on:consider={handleSort} on:finalize={handleSort} class="main-container">
     {#each words as word(word.id)} 
-    <WordBlock word={word.title} />
+    <WordBlock 
+        word={word.title} 
+        onActivate={() => handleBlockInteract(word)}
+    />
     {/each}
 </div>
 
--- crabble-web/src/lib/components/WordBlock.svelte
+++ crabble-web/src/lib/components/WordBlock.svelte
@@ -1,8 +1,18 @@
-<script>
-    let { word } = $props()
+<script lang="ts">
+  import { onMount } from "svelte";
+
+    let { word, onActivate = undefined, element = $bindable(undefined) } = $props()
+
+    onMount(() => {
+        element.addEventListener("keydown", (event: KeyboardEvent) => {
+            if (event.key == "Enter" && onActivate != undefined) {
+                onActivate()
+            }
+        })
+    })
 </script>
 
-<div class="word">
+<div class="word" bind:this={element}>
     {word}
 </div>
 
