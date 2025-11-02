52310caaf545c6f811b2e416e53a110213c55425
Alí Sinisterra <haidarconh@gmail.com>
2025-11-02 18:46:57 -0500

    Centralize puzzle-related info and logic into shared.svelte.ts (#4)
    move Word type to shared.svelte.ts
    
    remove onActivate from WordBlock
    
    separate puzzle from currentSolution and place on shared.svelte.ts
    
    make tiny code spacing changes to shared.svelte.ts and Shuffler

--- crabble-web/src/shared.svelte.ts
+++ crabble-web/src/shared.svelte.ts
@@ -1,14 +1,19 @@
 class AppEvents extends EventTarget {}
 export const appEvents = new AppEvents();
 
-export const swap = (array: Array<any>, indexA: number, indexB: number) => {
-  let result = array;
-
-  let indexAValue = array[indexA];
-  let indexBValue = array[indexB];
-
-  result[indexA] = indexBValue;
-  result[indexB] = indexAValue;
-
-  return result;
+export type Word = {
+  id: number;
+  title: string;
 };
+
+export let gameState = $state({
+  puzzle: [
+    { id: 1, title: "meow" },
+    { id: 2, title: "woo" },
+    { id: 3, title: "hoo" },
+  ] as Array<Word>,
+  currentSolution: [] as Array<Word>,
+});
+
+gameState.currentSolution = gameState.puzzle;
+
