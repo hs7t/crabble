a9f719ca4210c19fcd0bc6dc57a443560b58ceff
Alí Sinisterra <haidarconh@gmail.com>
2025-11-02 16:22:09 -0500

    Improve keyboard support (#3)
    create swap function in shared.svelte.ts
    
    add word swapping with keyboard in Shuffler
    
    add AppEvents EventTarget to shared.svelte.ts
    
    attempt further keyboard support
    
    use new solution for keyboard support that makes use of Q and W for word moving

--- crabble-web/src/lib/components/WordBlock.svelte
+++ crabble-web/src/lib/components/WordBlock.svelte
@@ -1,8 +1,34 @@
-<script>
-    let { word } = $props()
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { appEvents } from "../../shared.svelte";
+
+    let { word, onActivate = undefined, element = $bindable(undefined) } = $props()
+
+    onMount(() => {
+        element.addEventListener("keydown", (event: KeyboardEvent) => {
+            if (event.code == "Enter" && onActivate != undefined) {
+                onActivate()
+                element.focus()
+            }
+        })
+
+        element.addEventListener("keyup", (event: KeyboardEvent) => {
+            if (event.code == "KeyQ" || event.code == "KeyW" || event.code == "Enter") {
+                event.preventDefault()
+            }
+            appEvents.dispatchEvent(
+                new CustomEvent("wordBlockKeyUp", {
+                    detail: {
+                        receivedEvent: event,
+                        word: word,
+                    }
+                })
+            )
+        })
+    })
 </script>
 
-<div class="word">
+<div class="word" bind:this={element}>
     {word}
 </div>
 
