ce3234fa3fc367b5ccce60a8bae2a0b8a7883024
Alí Sinisterra <haidarconh@gmail.com>
2025-11-01 21:19:19 -0500

    Copy code to /crabnet (#2)
    copy github:hs7t/volnet into crabnet
    
    update Volumes reference in crabnet README.md

---
+++ crabnet/main.py
@@ -0,0 +1,39 @@
+import json
+from fastapi import FastAPI, HTTPException
+from wordalize import checkSimilarity
+from timekeeping import dateFromYMD
+
+app = FastAPI()
+
+async def readPuzzles():
+    with open('puzzles.json', 'r') as puzzlesJSON:
+        puzzles = json.load(puzzlesJSON)
+    return puzzles
+
+async def readLatestPuzzle():
+    puzzles = await readPuzzles()
+    latestPuzzle = next(reversed(puzzles.values()))
+
+    return latestPuzzle
+
+@app.get("/")
+async def main():
+    return "Hello!"
+
+@app.get("/v1/puzzles/latest")
+async def returnLatestPuzzle():
+    return await readLatestPuzzle()
+
+@app.get("/v1/similarity/{guess}")
+async def checkGuessCloseness(guess: str, solution: str|None = None): 
+    if solution == None:
+        solution = (await readLatestPuzzle())["word"]
+
+    return checkSimilarity(guess, solution)
+
+@app.get("/v1/puzzles/{time}")
+async def fetchPuzzle(time: str):
+    try:
+        return (await readPuzzles())[time]
+    except:
+        raise HTTPException(status_code=404, detail="Couldn't find puzzle")
