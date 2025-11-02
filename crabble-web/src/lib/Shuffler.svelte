73cfabf330ced999e2a2919a3b1e95f4aef785a1
Alí Sinisterra <haidarconh@gmail.com>
2025-11-01 21:01:19 -0500

    use svelte-dnd-action in Shuffler

--- crabble-web/src/lib/Shuffler.svelte
+++ crabble-web/src/lib/Shuffler.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
     import WordBlock from "./components/WordBlock.svelte";
+    import { dndzone } from 'svelte-dnd-action'
 
-    let words = ["meow", "woo", "hoo"]
+    let words = [{id: 1, title: "meow"}, {id: 2, title: "woo"}, {id: 3, title: "hoo"}]
+
+	function handleSort(e: any) {
+		words = e.detail.items;
+	}
+
 </script>
 
-<div class="main-container">
-    {#each words as word} 
-    <WordBlock {word} />
+<div use:dndzone="{{items: words}}" on:consider={handleSort} on:finalize={handleSort} class="main-container">
+    {#each words as word(word.id)} 
+    <WordBlock word={word.title} />
     {/each}
 </div>
 
@@ -16,5 +22,6 @@
         display: flex;
         flex-direction: row;
         gap: 1ch;
+        overflow-y: scroll;
     }
 </style>
