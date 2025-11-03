479d2d9a2b92d721f4c7de90a66633e1bc086e10
Haidar Alí Sinisterra Rojas <125828096+hs7t@users.noreply.github.com>
2025-11-03 13:49:22 -0500

    Work on game status (#8)
    * add recursion to shuffle() to avoid result == input
    
    * create, export isWon function
    
    * create Button component
    
    * create Dialog component
    
    * remove ul rule in Dialog
    
    * create getOverStatus function
    
    * export gameStatus type
    
    * return status in shared.svelte.ts
    
    * create GameOver component
    
    * show GameOver overlay when game is over on App.svelte
    
    * create B background colour
    
    * update variables used in Button to match
    
    * create new colour variables
    
    * update variables used in Dialog to match
    
    * move shown logic inside GameOver
    
    * fix isWon function

---
+++ crabble-web/src/lib/GameOver.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { gameStatus } from "../shared.svelte";
+    import Dialog from "./components/Dialog.svelte";
+    let { status = undefined as gameStatus|undefined} = $props()
+    let shown = $derived(status === 'lost' || status === 'won')
+</script>
+
+<Dialog {shown}>
+    {#snippet content()}
+        {#if status == 'lost'}
+            You lost :P
+        {/if}
+    {/snippet}
+</Dialog>
