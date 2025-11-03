0af0fe486f07fff4889ff6709f476b5c9f47dbe0
Alí Sinisterra <haidarconh@gmail.com>
2025-11-02 20:29:28 -0500

    remove event dispatcher from WordBlock

--- crabble-web/src/lib/components/WordBlock.svelte
+++ crabble-web/src/lib/components/WordBlock.svelte
@@ -1,24 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { appEvents } from "../../shared.svelte";
-
     let { word, element = $bindable(undefined), ...rest } = $props()
-
-    onMount(() => {
-        element.addEventListener("keyup", (event: KeyboardEvent) => {
-            if (event.code == "KeyQ" || event.code == "KeyW" || event.code == "Enter") {
-                event.preventDefault()
-            }
-            appEvents.dispatchEvent(
-                new CustomEvent("wordBlockKeyUp", {
-                    detail: {
-                        receivedEvent: event,
-                        word: word,
-                    }
-                })
-            )
-        })
-    })
 </script>
 
 <div class="word" bind:this={element} {...rest}>
