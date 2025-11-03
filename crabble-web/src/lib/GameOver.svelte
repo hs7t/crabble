2c6afc4e20f1e4767273e4fc8677318cd0193270
Alí Sinisterra <haidarconh@gmail.com>
2025-11-03 13:22:16 -0500

    create GameOver component

---
+++ crabble-web/src/lib/GameOver.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { gameStatus } from "../shared.svelte";
+    import Dialog from "./components/Dialog.svelte";
+    let { status = undefined as gameStatus|undefined } = $props()
+</script>
+
+<Dialog>
+    {#snippet content()}
+        {#if status == 'lost'}
+            You lost :P
+        {/if}
+    {/snippet}
+</Dialog>
