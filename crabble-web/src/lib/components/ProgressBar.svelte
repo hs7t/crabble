f78e4e20ec10ebf8d36622db147ecec4e6997731
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-04 17:30:56 -0500

    create ProgressBar component

---
+++ crabble-web/src/lib/components/ProgressBar.svelte
@@ -0,0 +1,22 @@
+<script>
+    let { currentValue, total } = $props()
+    let percentageElapsed = $derived( (currentValue / total) * 100 )
+</script>
+
+<div class="progress-bar" role="progressbar" aria-valuenow={percentageElapsed}>
+    <span class="progress" style="--current-percentage: {percentageElapsed}%;"></span>
+</div>
+
+<style>
+    .progress-bar {
+        width: 100%;
+        height: 1rem;
+    }
+    
+    .progress-bar .progress {
+        --current-percentage: 100%;
+
+        width: var(--current-percentage);
+        height: 100%;
+    }
+</style>
