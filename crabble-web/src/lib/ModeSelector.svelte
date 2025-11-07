0e850d531b86205900476ac3a25ed1075453285d
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-07 14:21:59 -0500

    start creating Picker and ModeSelector

---
+++ crabble-web/src/lib/ModeSelector.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Dialog from "./components/Dialog.svelte";
+    import Picker from "./components/Picker.svelte";
+
+    let { shown = $bindable(true) } = $props()
+</script>
+
+<Dialog bind:shown={shown}>
+    {#snippet content()}
+        <Picker></Picker>
+    {/snippet}
+</Dialog>
+
+<style>
+</style>
---
+++ crabble-web/src/lib/components/Picker.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div role="radiogroup">
+    <input type="radio" id="nameChoiceA" name="catName" value="michael">
+    <label for="nameChoiceA">Michael</label>
+
+    <input type="radio" id="nameChoiceB" name="catName" value="sam">
+    <label for="nameChoiceA">Sam</label>
+
+    <input type="radio" id="nameChoiceC" name="catName" value="taylor">
+    <label for="nameChoiceA">Taylor</label>
+</div>
