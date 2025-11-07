871110520e6a74e481dd5881aa59ce6ace7fd593
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-07 17:36:47 -0500

    use seconds instead of milliseconds in GameInfo, add basic styling

--- crabble-web/src/lib/GameInfo.svelte
+++ crabble-web/src/lib/GameInfo.svelte
@@ -5,18 +5,31 @@
     let timeLeft = $derived.by(() => {
         let value = 0
         if (gameState?.puzzleState) {
-            value = gameState.puzzleState.timeLeft
+            value = (gameState.puzzleState.timeLeft / 1000)
         }
         return value
     })
     let maxTime = $derived.by(() => {
         let value = 0
         if (gameState?.puzzleState) {
-            value = gameState.puzzleState.maxTime
+            value = (gameState.puzzleState.maxTime / 1000)
         }
         return value
     })
 </script>
 
-<ProgressBar currentValue={timeLeft} maxValue={maxTime}></ProgressBar>
-{timeLeft}
+<div class="progress">
+    <ProgressBar currentValue={timeLeft} maxValue={maxTime}></ProgressBar>
+    {timeLeft}
+</div>
+
+<style>
+    .progress {
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.8rem;
+    }
+</style>
