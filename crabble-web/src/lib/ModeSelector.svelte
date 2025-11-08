5f2f3ef80be7725a06c679dae988242275998e5a
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-08 11:53:44 -0500

    improve ModeSelector content

--- crabble-web/src/lib/ModeSelector.svelte
+++ crabble-web/src/lib/ModeSelector.svelte
@@ -34,28 +34,33 @@
                 ]}
                 bind:selectedValue={gameState.gameType}
             ></Picker>
+            {#if gameState.gameType == "lightning"}
+                <p><b>In Lightning mode</b>, you'll solve a set of three puzzles.</p>
+            {:else if gameState.gameType == "infinite"}
+                <p><b>In Infinite mode</b>, you can solve as many puzzles as you want.</p>
+            {:else if gameState.gameType == "spooky"}
+                <p>
+                    <b>Spooky mode</b> is Lightning mode, but spookier! Solve
+                    a set of three spooky-scary puzzles.
+                </p>
+
+            {/if}
+
         </section>
         <section>
-            <h3>How to play</h3>
-            <ul>
-                {#if gameState.gameType == "lightning"}
-                    <li><b>In Lightning mode</b>, you'll solve a set of three puzzles.</li>
-                {:else if gameState.gameType == "infinite"}
-                    <li><b>In Infinite mode</b>, you can solve as many puzzles as you want.</li>
-                {:else if gameState.gameType == "spooky"}
-                    <li>
-                        <b>Spooky mode</b> is Lightning mode, but spookier! In it, you'll solve
-                        a set of three spooky-scary puzzles.
-                    </li>
-
-                {/if}
-                <li>Complete a puzzle by putting each series of words in the correct order.</li>
-                <li>Use <code>Q</code> or <code>W</code> to select blocks.</li>
-                <li>Use <code>Space</code> to pick up a word. Then, use your arrow keys to move it.</li>
-                <li>Hit <code>Space</code> again to set down the word.</li>
-                <li>Not using a keyboard? Don't worry. Drag any word to set it in the right place.</li>
-            </ul>
+            <details>
+                <summary>How to play</summary>
+                <ul>
+                    <li>Complete a puzzle by putting each series of words in the correct order.</li>
+                    <li>Use <code>Q</code> or <code>W</code> to select blocks.</li>
+                    <li>Use <code>Space</code> to pick up a word. Then, use your arrow keys to move it.</li>
+                    <li>Hit <code>Space</code> again to set down the word.</li>
+                    <li>Not using a keyboard? Don't worry. Drag any word to set it in the right place.</li>
+                </ul>
+            </details>
         </section>
+    {/snippet}
+    {#snippet actions()}
         <Button action={handleGameStart} content="Start!"></Button>
     {/snippet}
 </Dialog>
